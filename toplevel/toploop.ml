02138959658658e0730e5cbf1485aba38b420689
pcouderc <pierrickcouderc@gmail.com>
2014-08-05 10:10:20 +0200

    How I reinvented the wheel : Subst already existed

--- toplevel/toploop.ml
+++ toplevel/toploop.ml
@@ -262,8 +262,8 @@ let execute_phrase print_outcome ppf phr =
                   match str.str_items with
                   | [ { str_desc = Tstr_eval (exp, _attrs) }] ->
                       let outv = outval_of_value newenv v exp.exp_type in
-                      let ty = Printtyp.tree_of_type_scheme exp.exp_type in
-                          (* (Typens.realias_type_expr exp.exp_type) in *)
+                      let ty = Printtyp.tree_of_type_scheme
+                          (Typens.realias_type_expr exp.exp_type) in
                       Ophr_eval (outv, ty)
                   | [] -> Ophr_signature []
                   | _ -> Ophr_signature (item_list newenv sg'))
