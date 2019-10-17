3567a40852d71d50f93cbe760776bfa61bc35617
Pierrick Couderc <pierrick.couderc@ocamlpro.com>
2019-10-17 14:58:22 +0200

    Support native embbedded functorized packs tests and fix testsuite

--- testsuite/tests/functorized-pack/embedded/test.ml
+++ testsuite/tests/functorized-pack/embedded/test.ml
@@ -2,7 +2,7 @@
 
 files="param.mli m1.ml m2.ml n1.ml n2.ml use_make.ml"
 
-* setup-ocamlc.byte-build-env
+* setup-ocamlopt.byte-build-env
 program = "${test_build_directory}/use_make.byte"
 ** ocamlc.byte
 flags = "-parameter-of Make"
@@ -42,6 +42,50 @@ module = ""
 program = "use_make.byte"
 flags = ""
 all_modules = "make.cmo use_make.cmo"
+************ run
+************* check-program-output
+reference = "${test_source_directory}/use_make.reference"
+
+* setup-ocamlopt.byte-build-env
+program = "${test_build_directory}/use_make.asm"
+** ocamlopt.byte
+flags = "-parameter-of Make"
+module = "param.mli"
+*** ocamlopt.byte
+flags = "-for-pack Make(Param).M"
+module = "m1.ml"
+**** ocamlopt.byte
+flags = "-for-pack Make(Param).M"
+module = "m2.ml"
+***** ocamlopt.byte
+flags = "-pack -for-pack Make(Param)"
+module = ""
+program = "m.cmx"
+all_modules = "m1.cmx m2.cmx"
+****** ocamlopt.byte
+flags = "-for-pack Make(Param).N"
+module = "n1.ml"
+******* ocamlopt.byte
+flags = "-for-pack Make(Param).N"
+module = "n2.ml"
+******** ocamlopt.byte
+flags = "-pack -for-pack Make(Param)"
+module = ""
+program = "n.cmx"
+all_modules = "n1.cmx n2.cmx"
+********* ocamlopt.byte
+flags = "-parameter Param -pack"
+module = ""
+program = "make.cmx"
+all_modules = "m.cmx n.cmx"
+********** ocamlopt.byte
+module = "use_make.ml"
+flags = ""
+*********** ocamlopt.byte
+module = ""
+program = "use_make.asm"
+flags = ""
+all_modules = "make.cmx use_make.cmx"
 ************ run
 ************* check-program-output
 reference = "${test_source_directory}/use_make.reference"
